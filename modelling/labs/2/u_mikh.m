40ca034c3ce09a68f5eb1613dc3878da01d3535e
gcdhg <gcdhg@outlook.com>
2017-03-23 11:42:48 +0300

    lab2 small changes

--- modelling/labs/2/u_mikh.m
+++ modelling/labs/2/u_mikh.m
@@ -1,15 +1,24 @@
-function f=u_mikh
+function f=u_mikh(A)
 n=3
-a0 = input('¬ведите а0=');
-a1 = input('¬ведите а1=');
-a2 = input('¬ведите а2=');
-a3 = input('¬ведите а3=');
+a0 = A(1);
+a1 = A(2);
+a2 = A(3);
+a3 = A(4);
+
+fuck = [];
+q = 1;
 
 maxx=0; maxy=0; minx=0; miny=0;
 for w=0.01:0.005:5,
     Njw= a0*((w*i)^3) + a1*((w*i)^2) + a2*(w*i) + a3;
     Re = real(Njw);
     Im = imag(Njw);
+    
+    if(Njw == 0)
+        fuck(q) = w;
+        q = q + 1;
+    end
+    
     if(Re>maxx)
         maxx=Re;
     end
@@ -25,6 +34,7 @@ for w=0.01:0.005:5,
     plot(Re, Im, 'x')
     hold on
 end
+fuck
 x=0; y=miny-20:0.1:maxy+20;
 plot(x,y, 'x')
 hold on
@@ -33,4 +43,6 @@ plot(x,y, 'x')
 hold off
 grid on
 axis([minx-20 maxx+20 miny-20 maxy+20])
+
+
 end
